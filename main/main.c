de3ee8407ad9d990d6339dfb24d33813dcff0324
ucflumm <ucflumm@gmail.com>
2024-06-21 21:58:47 +1000

    chore: Add initial project files and configuration

---
+++ main/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/semphr.h"
+#include "driver/gpio.h"
+#include "esp_freertos_hooks.h"
+#include "esp_system.h"
+
+#include "lvgl.h"
+
+void app_main(void)
+{
+
+}
